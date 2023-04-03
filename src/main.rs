df91a9b4e0cc4cf98e304225a58b8bd089245a4e
Kayky Casagrande <kayky.casagrande@unicred.com.br>
2023-04-03 09:49:52 -0300

    almost there...

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::fmt::Display;
+
+type Link<T> = Option<Box<Node<T>>>;
+
+struct Node<T> {
+    value: T,
+    next: Link<T>,
+}
+
+impl<T> Node<T> {
+    fn new(value: T, next: Link<T>) -> Self {
+        Self { value, next }
+    }
+}
+
+struct Stack<T> {
+    root: Link<T>,
+}
+
+impl<T> Stack<T>
+where
+    T: Display + Copy,
+{
+    fn new() -> Self {
+        Self { root: None }
+    }
+
+    fn is_empty(&self) -> bool {
+        self.root.is_none()
+    }
+
+    fn push(&mut self, value: T) {
+        self.root = Some(Box::new(Node::new(value, self.root)));
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
